26f5c601e6d8f55df488377aba525bb1cdf40f94
MasaYan24 <m.takahashi.public+github@gmail.com>
2018-08-24 14:50:42 +0900

    texasholdem_Dealer.py: add Card class

--- texasholdem_Dealer.py
+++ texasholdem_Dealer.py
@@ -2,9 +2,26 @@
 
 import sys
 
+class Card(object):
+    def __init__(self,suit,number):
+        if suit not in ("S","C","H","D"): # S: spade, C: club, H: heart, D: Diamond
+            raise ValueError("ERROR: suit of card is not correct: " + str(suit))
+        self.__suit = suit
+        if number not in range(1,14):
+            raise ValueError("ERROR: number of card is not correct: " + str(number))
+        self.__number = number
+
+    @property
+    def card(self):
+        return (self.__suit,self.__number)
+    @property
+    def suit(self):
+        return self.__suit
+    @property
+    def number(self):
+        return self.__number
 
 class Dealer(object):
     pass
-
 
 
