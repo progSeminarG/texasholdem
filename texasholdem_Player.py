e1c203df3d1a7e3a6ecf5138449b82f0ad063f96
Kawada-Yoshihiro <curly_hair_sprinter@i.softbank.jp>
2018-09-06 20:07:30 +0900

    修正版

--- texasholdem_Player.py
+++ texasholdem_Player.py
@@ -4,8 +4,8 @@ import random
 
 class Player(object):
     def get_know_dealer(self,dealer_input):
-        self.dealer = dealer_input   
+        self.dealer = dealer_input
     def get_hand(self,list_of_cards):
         self.cards = list_of_cards
-    def respond(self)
-        return random.choice([ ‘call’, ’fold’, int(10)])
+    def respond(self):
+        return random.choice([ 'call', 'fold', int(10)])
