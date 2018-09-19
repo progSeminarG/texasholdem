ef0e38db3af6022e97e6a8700e19b3df2289ac16
MasaYan24 <m.takahashi.public+github@gmail.com>
2018-09-19 23:28:25 +0900

    format around = is revised

--- texasholdem_Dealer.py
+++ texasholdem_Dealer.py
@@ -8,10 +8,10 @@ class Card(object):
     def __init__(self,suit,number):
         if suit not in ("S","C","H","D"): # S: spade, C: club, H: heart, D: Diamond
             raise ValueError("ERROR: suit of card is not correct: " + str(suit))
-        self.__suit = suit
+        self.__suit  =  suit
         if number not in range(1,14):
             raise ValueError("ERROR: number of card is not correct: " + str(number))
-        self.__number = number
+        self.__number  =  number
 
     @property
     def card(self):
@@ -25,23 +25,23 @@ class Card(object):
 
 class Dealer(object):
     def __init__(self,players_input):
-        self.__MIN_NUMBER_CARDS = 1 # smallest number of playing cards
-        self.__MAX_NUMBER_CARDS = 13 # largest number of playing cards
-        self.__SUITE = ['S','C','H','D'] # suit of playing cards
-        self.__NUM_HAND = 2 # number of hands
-        self.__INITIAL_MONEY = 500 # money each player has in initial
-        self.__NUM_MAX_FIELD = 5 # maximum number of field
-        self.__players = players_input # instance of players
-        self.__num_players = len(self.__players) # number of players
-        self.__num_handling_cards = self.__NUM_HAND * self.__num_players + self.__NUM_MAX_FIELD # number of cards that deal with
-        self.__money_each_player = [self.__INITIAL_MONEY]*self.__num_players # money list of players
-        self.__field = []
+        self.__MIN_NUMBER_CARDS  =  1 # smallest number of playing cards
+        self.__MAX_NUMBER_CARDS  =  13 # largest number of playing cards
+        self.__SUITE  =  ['S','C','H','D'] # suit of playing cards
+        self.__NUM_HAND  =  2 # number of hands
+        self.__INITIAL_MONEY  =  500 # money each player has in initial
+        self.__NUM_MAX_FIELD  =  5 # maximum number of field
+        self.__players  =  players_input # instance of players
+        self.__num_players  =  len(self.__players) # number of players
+        self.__num_handling_cards  =  self.__NUM_HAND * self.__num_players + self.__NUM_MAX_FIELD # number of cards that deal with
+        self.__money_each_player  =  [self.__INITIAL_MONEY]*self.__num_players # money list of players
+        self.__field  =  []
         for player in self.__players:
             player.get_know_dealer(self)
 
 
     def __create_all_cards_stack(self): # create list of [S1, S2, ..., D13]
-        _cards = []
+        _cards  =  []
         for inumber in range(self.__MIN_NUMBER_CARDS,self.__MAX_NUMBER_CARDS+1):
             for suit in self.__SUITE:
                 _cards.append(Card(suit,inumber))
@@ -49,10 +49,10 @@ class Dealer(object):
 
 
     def handout_cards(self):
-        self.__field = []
-        self.__all_cards = self.__create_all_cards_stack()
-        self.__handling_cards = random.sample(self.__all_cards,self.__num_handling_cards)
-        self.__players_cards = [] # each player's hand
+        self.__field  =  []
+        self.__all_cards  =  self.__create_all_cards_stack()
+        self.__handling_cards  =  random.sample(self.__all_cards,self.__num_handling_cards)
+        self.__players_cards  =  [] # each player's hand
         for player in self.__players:
             self.__players_cards.append([self.__handling_cards.pop(i) for i in range(self.__NUM_HAND)])
             player.get_hand(self.__players_cards[-1])
@@ -61,45 +61,45 @@ class Dealer(object):
         self.__field.append(self.__handling_cards.pop(0))
 
     def get_response(self):
-        if len(self.field)==0: # 手札 2 枚の時に 1 度訊くのでそのときだけ minimum_bet と call の金額決める
-            self.money=2
-            self.minimum_bet=1
-            self.playercheck=[1]*len(self.__players) # 返答を毎度更新し、降りた時に０にする
-            self.active_plyers_list=[]
-            self.bettingrate=[0]*len(self.__players) # 各々が賭けたお金を記録するリスト
-        self.flag=0
-        while self.flag<len(self.__players) and len(self.active_plyers_list)!=1: # while文で flag がプレイヤー数になるという次の工程に移行する条件を定義
-            self.resplist=[]
-            self.active_plyers_list=[]
-            self.__resp=[
+        if len(self.field) == 0: # 手札 2 枚の時に 1 度訊くのでそのときだけ minimum_bet と call の金額決める
+            self.money = 2
+            self.minimum_bet = 1
+            self.playercheck = [1]*len(self.__players) # 返答を毎度更新し、降りた時に０にする
+            self.active_plyers_list = []
+            self.bettingrate = [0]*len(self.__players) # 各々が賭けたお金を記録するリスト
+        self.flag = 0
+        while self.flag<len(self.__players) and len(self.active_plyers_list)!= 1: # while 文で flag がプレイヤー数になるという次の工程に移行する条件を定義
+            self.resplist = []
+            self.active_plyers_list = []
+            self.__resp = [
                     self.resplist.append(player.respond()) for player in self.__players
                     ]
             for i in range (0,len(self.__players)): # flag でレイズから次にレイズがあるまでカウントししている
-                if self.flag>=len(self.__players):
+                if self.flag>= len(self.__players):
                     pass
-                elif self.resplist[i]=="fold":
-                    self.playercheck[i]=0
-                    self.flag=self.flag+1
-                elif self.resplist[i]=="call":
-                    self.flag=self.flag+1
-                    if self.playercheck[i]==1:
-                        self.bettingrate[i]=self.money
+                elif self.resplist[i] == "fold":
+                    self.playercheck[i] = 0
+                    self.flag = self.flag+1
+                elif self.resplist[i] == "call":
+                    self.flag = self.flag+1
+                    if self.playercheck[i] == 1:
+                        self.bettingrate[i] = self.money
                 else:
-                    if self.playercheck[i]==1: # レイズしてきたプレーヤが本当にさんかしつづけているか？
+                    if self.playercheck[i] == 1: # レイズしてきたプレーヤが本当にさんかしつづけているか？
                         if self.minimum_bet>self.resplist[i]: # minimum_bet より小さい金額なら minimum_bet に修正
-                            self.resplist[i]=self.minimum_bet
+                            self.resplist[i] = self.minimum_bet
                             # もうお金が無くて ALL_IN したい場合を追加予定
                         else:
-                            self.minimum_bet=self.resplist[i] # minimum_betの更新
-                        self.money=self.money+self.resplist[i] # call金額の更新
-                        self.flag=1
-                        self.bettingrate[i]=self.money
+                            self.minimum_bet = self.resplist[i] # minimum_bet の更新
+                        self.money = self.money+self.resplist[i] # call 金額の更新
+                        self.flag = 1
+                        self.bettingrate[i] = self.money
                     else:
-                        self.flag=self.flag+1
+                        self.flag = self.flag+1
 
 
             for i in range (0,len(self.__players)): # 降りなかった人をリストで返す
-                if self.playercheck[i]==1:
+                if self.playercheck[i] == 1:
                     self.active_plyers_list.append('Player'+str(i+1))
 
         print ("next_turn_players_list",[self.active_plyers_list]) # 次のターン参加する人のリスト
