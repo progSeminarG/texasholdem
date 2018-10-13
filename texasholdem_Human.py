7ab8a7291cc36dba80809fe177c57e898837a0d0
SHIRAI Mizuho <39171201+mshirai0327@users.noreply.github.com>
2018-10-13 20:53:50 +0900

    call,fold,money[int]をマニュアル入力するものを作りました．

---
+++ texasholdem_Human.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+import random
+
+class Player(object):
+    def get_know_dealer(self,dealer_input):
+        self.dealer = dealer_input
+    def get_hand(self,list_of_cards):
+        self.cards = list_of_cards
+    def open_cards(self):
+        return self.cards
+    def respond(self):
+        return random.choice([ 'call', 'fold', int(10)])
+        
+class Human(Player):
+    def respond(self):
+        cl=[]
+        for i in range(len(self.cards)):
+            cl.append(self.cards[i].card)
+        print("cards:",cl)
+        print("Please input:'call','fold',or money[int]-->")
+        res = input()
+        return res
