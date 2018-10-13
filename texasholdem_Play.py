43ca1b11fbe7cb933ba1e575d93ead348390d720
SHIRAI Mizuho <39171201+mshirai0327@users.noreply.github.com>
2018-10-13 20:05:34 +0900

    players_listを表示するようにしました

--- texasholdem_Play.py
+++ texasholdem_Play.py
@@ -7,6 +7,7 @@ from copy import deepcopy
 from texasholdem_Dealer import Card, Dealer
 from texasholdem_Player import Player
 from texasholdem_Kawada import KawadaAI
+from texasholdem_Human import Human
 
 
 class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
@@ -35,66 +36,39 @@ args = parser.parse_args()
 class Game(object):
     def __init__(self, players_list):
         self.__players = players_list
-        self.syozikin = [500]*len(self.__players)  # player's money at first
-        self.smallb = 0  # number of small-blined at first
 
     def play(self):
-        self.__dealer = Dealer(deepcopy(self.__players), deepcopy(self.syozikin), deepcopy(self.smallb))
+        self.__dealer = Dealer(deepcopy(self.__players))
         self.__dealer.handout_cards()
-        self.__dealer.get_responses()
+        self.__dealer.get_response()
         for i in range(3):
             self.__dealer.put_field()
         print("field:", [card.card for card in self.__dealer.field])
-        self.__dealer.get_responses()
+        self.__dealer.get_response()
         self.__dealer.put_field()
         print("field:", [card.card for card in self.__dealer.field])
-        self.__dealer.get_responses()
+        self.__dealer.get_response()
         self.__dealer.put_field()
         print("field:", [card.card for card in self.__dealer.field])
-        self.__dealer.get_responses()
-        print()
-        print("open cards && calculate score")
-        self.__dealer.calc()
-        self.syozikin = self.__dealer.syozikin_kosin()
-        self.smallb = (self.__dealer.smallb_kosin()+1)%len(self.__players)
-
-    def ninzu_kakunin(self):  # if the numer of players who can play new game is 1 return True
-        if self.__dealer.sanka_kano_ninzu() == 1:
-            return True
-        else:
-            return False
+        self.__dealer.get_response()
+        self.__dealer.calc_hand_score()
 
 
 # === create players ===
 player1 = KawadaAI()
-player2 = Player()
+player2 = Human()
 player3 = Player()
 player4 = Player()
 
 players_list = [player1, player2, player3, player4]
 random.shuffle(players_list)
-'''
+
 game = Game(players_list)
-NUM_GAME = 1
-for i in range(0,len(players_list)):
-    print(players_list[i])  # すみません毎回リストがシャッフルされて自分のAIが見づらいので書き足しました
+NUM_GAME = 5
+nl=[]
+for i in range(len(players_list)):
+    nl.append(players_list[i].__class__.__name__)
+print("players:",nl)  # すみません毎回リストがシャッフルされて自分のAIが見づらいので書き足しました
 for i in range(NUM_GAME):
     print("===== game", i, "=====")
-    game.play()'''
-
-
-game = Game(players_list)
-for i in range(0,len(players_list)):
-    print(players_list[i])  # すみません毎回リストがシャッフルされて自分のAIが見づらいので書き足しました
-game_end = False
-i = 0
-while game_end == False:
-    print("===== game", i, "=====")
     game.play()
-    game_end = game.ninzu_kakunin()
-    i = i+1
-    print()
-    print("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
-print(game.syozikin)
-for i in range(0, len(players_list)):
-    print(players_list[i])  # すみません毎回リストがシャッフルされて自分のAIが見づらいので書き足しました
