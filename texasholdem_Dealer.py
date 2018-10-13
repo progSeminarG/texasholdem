08657c0209f443afe814675e606a7fbecc079bfd
mshirai0327 <nnshiro@gmail.com>
2018-10-13 22:26:06 +0900

    st,flash判定のデバグ，コンフリクト修正．

--- texasholdem_Dealer.py
+++ texasholdem_Dealer.py
@@ -280,11 +280,7 @@ class Dealer(object):
         ##REPLACE 1-->14
         num=self.rpc1(num)
         num.sort()
-        nc=[]
-        for i in range(len(card_list)):
-            ss=card_list[i][0]
-            nn=(card_list[i][1]+11)%13 + 2
-            nc.append((ss,nn))
+        nc=self.rpcards1(card_list)
         card_list=nc
         card_list=sorted(card_list, key=lambda x: x[1])#2ndでsort
 
@@ -423,11 +419,7 @@ class Dealer(object):
             rtCards=card_list[2:7]
 
         ##RETURN!!##
-        nc=[]
-        for i in range(len(rtCards)):
-            ss=rtCards[i][0]
-            nn=(rtCards[i][1]-1)%13+1
-            nc.append((ss,nn))
+        nc=self.rpcards2(rtCards)
         rtCards=nc
         return (score,rtCards)
 
@@ -490,12 +482,29 @@ class Dealer(object):
             rp.append(card)
         return rp
 
-    def rpc2(self,cards):#最後に14-->1に戻す方
+    def rpc2(self,cards):#最後に14-->1に戻す方　引数はリスト
         rp=[]
         for card in cards:
             card=(card-1)%13+1
             rp.append(card)
         return rp
+        
+    def rpcards1(self,cards)#最初に1-->14にする方　引数はカードタプルリスト
+        nc=[]
+        for i in range(len(cards)):
+            ss=cards[i][0]
+            nn=(cards[i][1]+11)%13+2
+            nc.append((ss,nn))
+        return nc
+
+    def rpcards2(self,cards)#最後に14-->1に戻す方　引数はカードタプルリスト
+        nc=[]
+        for i in range(len(cards)):
+            ss=cards[i][0]
+            nn=(cards[i][1]-1)%13+1
+            nc.append((ss,nn))
+        return nc
+
 ### for: calc_hand_score ###
 ############################
 
@@ -505,6 +514,8 @@ class Dealer(object):
         for k in range(5):
             num1.append(cl1[k][1])
             num2.append(cl2[k][1])
+        self.rpc1(num1)
+        self.rpc1(num2)
         for i in range(5):
             if max(num1)>max(num2):
                 sc=0
@@ -524,11 +535,13 @@ class Dealer(object):
         for i in range(5):
             num1.append(cl1[i][1])
             num2.append(cl2[i][1])
-        if max(cl1)>max(cl2):
+        num1=self.rpc1(num1)
+        num2=self.rpc1(num2)
+        if max(num1)>max(num2):
             sc=0
-        elif max(cl1)<max(cl2):
+        elif max(num1)<max(num2):
             sc=1
-        elif max(cl1)==max(cl2):
+        elif max(num1)==max(num2):
             sc=2
         return sc
 
