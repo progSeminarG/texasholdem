3f402bee56cfb9110029817985d01dd1270ad525
masahiro <m.takahashi.public@gmail.com>
2018-10-15 10:19:32 +0900

    Play: noshuffle option added. see texasholdom_Play.py --help

--- texasholdem_Play.py
+++ texasholdem_Play.py
@@ -10,27 +10,6 @@ from texasholdem_Kawada import KawadaAI
 from texasholdem_Human import Human
 
 
-class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
-                      argparse.MetavarTypeHelpFormatter):
-    pass
-
-
-parser = argparse.ArgumentParser(description="play Texas Hold'em.",
-                                 formatter_class=CustomFormatter)
-
-'''
-parser.add_argument('--num', type=int, dest='num_game', nargs='?',
-default=1, help="number of game")
-parser.add_argument('--out', type=str, dest='outfile', nargs='?',
-default='stat.csv', help="output file")
-parser.add_argument('--fig', type=str, dest='figfile', nargs='?',
-default='stat.png', help="output figure file (png)")
-parser.add_argument('-q', '--quiet', action="store_true",
-help='reduce print sequence')
-parser.add_argument('--upload', type=str, dest='token', nargs=1,
-help='upoad figure. parse token.')
-'''
-args = parser.parse_args()
 
 
 class Game(object):
@@ -77,45 +56,72 @@ class Game(object):
         return self.__DBTN
 
 
-# === create players ===
-player1 = KawadaAI()
-player2 = Human()
-# player2 = Player()
-player3 = Player()
-player4 = Player()
-
-players_list = [player1, player2, player3, player4]
-random.shuffle(players_list)
+if __name__ == '__main__':
+    class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
+                      argparse.MetavarTypeHelpFormatter):
+        pass
 
 
+    parser = argparse.ArgumentParser(description="play Texas Hold'em.",
+                                     formatter_class=CustomFormatter)
 
-game = Game(players_list)
-nl = []
-for i in range(0,len(players_list)):
-    nl.append(players_list[i].__class__.__name__)
-print("players;", nl)
+    parser.add_argument('--noshuffle', action='store_false', dest='shuffle',
+                        help='do not shuffle players')
 
-NUM_GAME = 5
+    '''
+    parser.add_argument('--num', type=int, dest='num_game', nargs='?',
+    default=1, help="number of game")
+    parser.add_argument('--out', type=str, dest='outfile', nargs='?',
+    default='stat.csv', help="output file")
+    parser.add_argument('--fig', type=str, dest='figfile', nargs='?',
+    default='stat.png', help="output figure file (png)")
+    parser.add_argument('-q', '--quiet', action="store_true",
+    help='reduce print sequence')
+    parser.add_argument('--upload', type=str, dest='token', nargs=1,
+    help='upoad figure. parse token.')
+    '''
+    args = parser.parse_args()
 
-for i in range(NUM_GAME):
-    print("===== game", i, "=====")
-    game.play()
+    # === create players ===
+    player1 = KawadaAI()
+    player2 = Human()
+    # player2 = Player()
+    player3 = Player()
+    player4 = Player()
 
-'''
-game = Game(players_list)
-nl = []
-for i in range(0,len(players_list)):
-    nl.append(players_list[i].__class__.__name__)
-print("players;", nl)
-game_end = False
-i = 0
-while game_end == False:
-    print("===== game", i, "=====")
-    game.play()
-    game_end = game.ninzu_kakunin()
-    i = i+1
-    print()
-    print("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
-print(game.syozikin)
-print("players;", nl)  # すみません毎回リストがシャッフルされて自分のAIが見づらいので書き足しました
-'''
+    players_list = [player1, player2, player3, player4]
+    if args.shuffle:
+        random.shuffle(players_list)
+
+
+
+    game = Game(players_list)
+    nl = []
+    for i in range(0,len(players_list)):
+        nl.append(players_list[i].__class__.__name__)
+    print("players;", nl)
+
+    NUM_GAME = 5
+
+    for i in range(NUM_GAME):
+        print("===== game", i, "=====")
+        game.play()
+
+    '''
+    game = Game(players_list)
+    nl = []
+    for i in range(0,len(players_list)):
+        nl.append(players_list[i].__class__.__name__)
+    print("players;", nl)
+    game_end = False
+    i = 0
+    while game_end == False:
+        print("===== game", i, "=====")
+        game.play()
+        game_end = game.ninzu_kakunin()
+        i = i+1
+        print()
+        print("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
+    print(game.syozikin)
+    print("players;", nl)  # すみません毎回リストがシャッフルされて自分のAIが見づらいので書き足しました
+    '''
