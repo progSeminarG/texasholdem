c94f8a6579594a2db29d9a2f521ca4f47e6976d8
SHIRAI Mizuho <39171201+mshirai0327@users.noreply.github.com>
2019-03-03 23:37:08 -0500

    update

--- Shiraiopt/init.py
+++ Shiraiopt/init.py
@@ -2,27 +2,29 @@ import random
 import numpy as np
 import os
 
+name=[0]*5
+pname=[0]*5
 mat=[0]*5
-mat[0]=np.zeros((9,10))
+plusmat=[0]*5
+for i in range(5):
+    name[i] = "mat" + str(i) + ".npy"
+    pname[i] = "plusmat" + str(i) + ".npy"
+
+mat[0]=np.zeros((10,10))
 mat[1]=np.zeros((10,10))
 mat[2]=np.zeros((10,10))
 mat[3]=np.zeros((10,10))
 mat[4]=np.zeros((10,3))
 
-plusmat=[0]*5
-plusmat[0]=np.zeros((9,10))
+plusmat[0]=np.zeros((10,10))
 plusmat[1]=np.zeros((10,10))
 plusmat[2]=np.zeros((10,10))
 plusmat[3]=np.zeros((10,10))
 plusmat[4]=np.zeros((10,3))
 
-np.save('mat.npy',mat)
-print(np.load('mat.npy'))
-
-np.save('plusmat.npy',plusmat)
-print(np.load('plusmat.npy'))
-
+for i in range(5):
+    np.save(name[i],mat[i])
+    np.save(pname[i],plusmat[i])
 
 presc=100
 np.save('presc.npy',presc)
-print(np.load('presc.npy'))
