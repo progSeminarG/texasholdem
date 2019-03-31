3b2a2533b5bb2739add12bdde4f05774b135272e
MasaYan24 <m.takahashi.public+github@gmail.com>
2019-03-31 11:46:25 +0900

    Player: add sample of get_result

--- texasholdem_Player.py
+++ texasholdem_Player.py
@@ -3,9 +3,12 @@
 import random
 
 class Player(object):
-    def get_know_dealer(self,dealer_input):
+    def get_know_dealer(self,dealer_input):  # necessary
         self.dealer = dealer_input
-    def get_hand(self,list_of_cards):
+    def get_hand(self,list_of_cards):  # necessary
         self.cards = list_of_cards
-    def respond(self):
+    def respond(self):  # necessary
         return random.choice([ 'call', 'fold', int(10)])
+    def get_result(self,result):  # called when 1 game finished
+        pass
+        # print(result)
